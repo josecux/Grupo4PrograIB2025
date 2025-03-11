fb314a6fafc69790dbddfa5a833ed55953b9da06
yo <yo@DESKTOP-GME44Q9>
2025-03-11 20:39:09 +0100

    Initial commit

---
+++ Grupo4PrograIB2025/src/main/java/gt/edu/miumg/grupo4prograib2025/Grupo4PrograIB2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gt.edu.miumg.grupo4prograib2025;
+
+/**
+ *
+ * @author yo
+ */
+public class Grupo4PrograIB2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
